78106f08ba8e31894a0b509e862d50e0ce0cbea1
Bolin Wu <bolin.wu1026@gmail.com>
2023-01-30 18:49:09 +0100

    give fake data random NA and update slides

--- rpackage/data-raw/fake_snack_df.R
+++ rpackage/data-raw/fake_snack_df.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(sjlabelled)
 library(lubridate)
+library(missMethods)
 
 # read fake SNAC-K data
 fake_snack_df = haven::read_dta("data-raw/snack_random.dta")
@@ -15,6 +16,11 @@ fake_snack_df <- fake_snack_df %>%
   mutate(across(where(is.Date), ~ sample(seq(as.Date("1700/01/01"), as.Date("1850/01/01"), by = "day"), size = length(.)))) %>%
   mutate(Lopnr = sample.int(nrow(fake_snack_df))) %>%
   set_label(label = label_names)
+
+# generate random NA
+set.seed(2023)
+fake_snack_df <- delete_MCAR(fake_snack_df,
+                                         p = seq(0.1, 0.8, length.out = length(fake_snack_df)))
 
 
 usethis::use_data(fake_snack_df, overwrite = TRUE)
