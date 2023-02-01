b780c9d365c0286027635a4ad8370679a838c56d
Bolin Wu <bolin.wu1026@gmail.com>
2023-02-01 19:20:36 +0100

    update fake data documentation

--- rpackage/R/data.R
+++ rpackage/R/data.R
@@ -1,7 +1,8 @@
 #' Fake data
 #'
-#' A simulated fake data set created by Xin Xia.
+#' A simulated fake data set created by Xin Xia. It has mmse, age, date, education, dementia, etc.
 #'
 #'
-#' @format A data frame with 28 variables and 3000 random obs.
+#' @format A data frame with 28 variables and 3000 simulated obs.
+#' @source Xin Xia's simulation.
 "fake_data"
