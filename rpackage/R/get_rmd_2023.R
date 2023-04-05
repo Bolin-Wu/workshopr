09ece9710268a605865e432ab788e592e91638b3
Bolin Wu <bolin.wu1026@gmail.com>
2023-04-05 18:52:02 +0200

    add a pdf template and made minor changes in the ggplot

--- rpackage/R/get_rmd_2023.R
+++ rpackage/R/get_rmd_2023.R
@@ -1,21 +1,21 @@
 #' @title get_rmd_2023
 #' @description Use the preset rmd template. By default, this function creates a new folder "rmd" at root of project and create an rmd file with nice-looking template there.
-#' @param name Name of rmd file. Please do not use '/' in the the file name. (no need to add suffix ".Rmd")
+#' @param name Name of rmd file. Please do not use '/' in the the file name (no need to add suffix ".Rmd").
 #' @param subDir Name of the folder where rmd file will be created. Remember to connect sub-dir names with '/'. By default it is 'rmd'.
-#' @param open Should the file be opened after being created
-#' @param output_file "word" or "html", by default it is "html"
+#' @param open Should the file be opened after being created.
+#' @param output_file "word" or "html" or "pdf", by default it is "html".
 #' @param ... Arguments to be passed to \link[usethis]{use_template}
 #' @importFrom usethis use_template
 #' @examples
 #' \dontrun{
-#'  workshopr::get_rmd_2023(subDir = 'rmd',name = "pretty_template",output_file = "word")
+#' workshopr::get_rmd_2023(subDir = "rmd", name = "pretty_template", output_file = "word")
 #' }
 #'
 #' @export
 #'
 get_rmd_2023 <-
   function(name = NULL,
-           subDir = 'rmd',
+           subDir = "rmd",
            output_file = "html",
            open = interactive(),
            ...) {
@@ -27,26 +27,32 @@ get_rmd_2023 <-
 
     # create a folder for Rmd
     if (dir.exists(subDir)) {
-      message("folder:", subDir," already exists.")
+      message("folder:", subDir, " already exists.")
     } else {
       message("Create folder: ", subDir, ".")
-      dir.create(subDir,  showWarnings = TRUE, recursive = TRUE)
+      dir.create(subDir, showWarnings = TRUE, recursive = TRUE)
     }
 
     # usethis::use_package("usethis")
     if (output_file == "html") {
       usethis::use_template("rmarkdown/skeleton_html.Rmd",
-                            save_as = paste0(subDir, "/",name),
-                            data = list(),
-                            package = "workshopr", ..., open = open
+        save_as = paste0(subDir, "/", name),
+        data = list(),
+        package = "workshopr", ..., open = open
       )
     } else if (output_file == "word") {
       usethis::use_template("rmarkdown/skeleton_word.Rmd",
-                            save_as = paste0(subDir, "/",name),
-                            data = list(),
-                            package = "workshopr", ..., open = open
+        save_as = paste0(subDir, "/", name),
+        data = list(),
+        package = "workshopr", ..., open = open
+      )
+    } else if (output_file == "pdf") {
+      usethis::use_template("rmarkdown/skeleton_pdf.Rmd",
+        save_as = paste0(subDir, "/", name),
+        data = list(),
+        package = "workshopr", ..., open = open
       )
     } else {
-      stop("The 'output_file' should be 'html' or 'word'")
+      stop("The 'output_file' should be 'html' or 'word' or 'pdf'.")
     }
   }
